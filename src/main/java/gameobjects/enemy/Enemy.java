69e93a8f821d8141a42128536e0530a187ceef04
Ethan Senger <esenger02@icloud.com>
2024-11-18 14:42:32 -0800

    update 2

--- src/main/java/gameobjects/enemy/Enemy.java
+++ src/main/java/gameobjects/enemy/Enemy.java
@@ -23,6 +23,48 @@ public class Enemy {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
 
+    public int getHealth() {
+        return health;
+    }
 
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getMaxHealth() {
+        return maxHealth;
+    }
+
+    public void setMaxHealth(int maxHealth) {
+        this.maxHealth = maxHealth;
+    }
+
+    public int getDefense() {
+        return defense;
+    }
+
+    public void setDefense(int defense) {
+        this.defense = defense;
+    }
+
+    public int getAttackDamage() {
+        return attackDamage;
+    }
+
+    public void setAttackDamage() {
+        this.attackDamage = attackDamage;
+    }
+
+    public boolean enemyIsDead() {
+        return health == 0;
+    }
+
+    public int enemyAttack () {
+        int damage = getAttackDamage();
+        return damage;
+    }
 }
